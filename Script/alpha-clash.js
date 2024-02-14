062d2a05d0566d222e1a3b4cf2545780f50e4aef
Farhana Yeasmin Munmun <farhanayeasminmunmun@gmail.com>
2024-02-14 19:06:40 +0600

    background color add

--- Script/alpha-clash.js
+++ Script/alpha-clash.js
@@ -1,11 +1,26 @@
-// alternative way
+function continueGame(){
+    // generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet',alphabet);
 
+    // set alphabet
+
+    const currentAlphabetElement = document.getElementById('randomAlphabet');
+    currentAlphabetElement.innerText =alphabet;
+
+    // set background color 
+    setBackgroundColorById(alphabet);
+
+}
 
 function play() {
 
     hideElementById('home');
     showElementById('play');
+    continueGame();
 }
+
+// alternative way
     //     // hide the home screen
     //     const homeSection = document.getElementById('home');
     //     homeSection.classList.add('hidden');
--- Script/utility.js
+++ Script/utility.js
@@ -6,3 +6,27 @@ function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400')
+    
+
+}
+
+function getARandomAlphabet() {
+    // create random alphabet
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    // get random alphabet (index 0-25)
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    console.log(index,alphabet);
+    return alphabet;
+
+    
+}
