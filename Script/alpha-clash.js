6c2845a9e2c8b19814676447ae15e4e98668069f
Farhana Yeasmin Munmun <farhanayeasminmunmun@gmail.com>
2024-02-14 14:28:57 +0600

    show hidden done

---
+++ Script/alpha-clash.js
@@ -0,0 +1,20 @@
+// alternative way
+
+
+function play() {
+
+    hideElementById('home');
+    showElementById('play');
+}
+    //     // hide the home screen
+    //     const homeSection = document.getElementById('home');
+    //     homeSection.classList.add('hidden');
+
+    //     // show the playground
+    //     const playGroundSection = document.getElementById('play');
+    //     playGroundSection.classList.remove('hidden');
+
+
+
+
+
