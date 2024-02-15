7abdfd9467f1824785353e11ee2ffd61cb98f636
Farhana Yeasmin Munmun <farhanayeasminmunmun@gmail.com>
2024-02-15 14:57:14 +0600

    unfinished

--- Script/alpha-clash.js
+++ Script/alpha-clash.js
@@ -1,12 +1,40 @@
-function continueGame(){
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    console.log('Player pressed', playerPressed);
+
+    //get the expected to press key 
+    const currentAlphabetElement = document.getElementById('randomAlphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    // checked matching key
+    if(playerPressed===expectedAlphabet){
+        console.log('you get a point');
+        console.log('You have press correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+
+    }
+    else{
+        console.log("you lost a life.");
+    }
+
+
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+
+function continueGame() {
     // generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('Your random alphabet',alphabet);
+    // console.log('Your random alphabet', alphabet);
 
     // set alphabet
 
     const currentAlphabetElement = document.getElementById('randomAlphabet');
-    currentAlphabetElement.innerText =alphabet;
+    currentAlphabetElement.innerText = alphabet;
 
     // set background color 
     setBackgroundColorById(alphabet);
@@ -21,13 +49,13 @@ function play() {
 }
 
 // alternative way
-    //     // hide the home screen
-    //     const homeSection = document.getElementById('home');
-    //     homeSection.classList.add('hidden');
+//     // hide the home screen
+//     const homeSection = document.getElementById('home');
+//     homeSection.classList.add('hidden');
 
-    //     // show the playground
-    //     const playGroundSection = document.getElementById('play');
-    //     playGroundSection.classList.remove('hidden');
+//     // show the playground
+//     const playGroundSection = document.getElementById('play');
+//     playGroundSection.classList.remove('hidden');
 
 
 
--- Script/utility.js
+++ Script/utility.js
@@ -9,12 +9,12 @@ function showElementById(elementId) {
 
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+    element.classList.add('bg-orange-400');
     
 
 }
 
-function removeBackgroundColor(elementId){
+function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 
